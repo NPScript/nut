0256677efe273e9cd2854799a1731e03e884e862
NPScript <nathan@reinerweb.ch>
2021-03-06 17:25:14 +0100

    gcol update

--- tools/gcol.c
+++ tools/gcol.c
@@ -1,32 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char stdin_line[BUFSIZ];
 unsigned col;
-
-inline static void get_col() {
-	int count = 1;
-	char * c = stdin_line;
-
-	for (;count != col && *c != '\n'; ++c) {
-		if (*c == ' ' || *c == '\t') {
-			++count;
-			for (;*c == ' ' || *c == '\t'; ++c);
-			--c;
-		}
-	}
-
-	for (; *c != ' ' && *c != '\t' && *c !='\n'; ++c) {
-		putc(*c, stdout);
-	}
-	putc('\n', stdout);
-}
-
-inline static void read_stdin() {
-	while (fgets(stdin_line, sizeof stdin_line, stdin)) {
-		get_col();
-	}
-}
+char c;
+int count;
 
 int main(int argc, char ** argv) {
 	if (argc != 2) {
@@ -36,7 +13,26 @@ int main(int argc, char ** argv) {
 
 	col = atoi(argv[1]);
 
-	read_stdin();
+	c = getchar();
+
+	while (c != EOF) {
+		count = 1;
+		while (c != '\n') {
+			if (c == ' ' || c == '\t') {
+				++count;
+				while ((c = getchar()) == ' ' || c == '\t');
+				continue;
+			} else if (count == col) {
+				putc(c, stdout);
+			} else if (count > col) {
+				while (getchar() != '\n');
+				break;
+			}
+			c = getchar();
+		}
+		putc('\n', stdout);
+		c = getchar();
+	}
 
 	return 0;
 }
